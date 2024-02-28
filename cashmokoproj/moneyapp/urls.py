bdbaaa4e048356a5e9a9a11b806585b3b9992713
Joshua Felipe <jlfelipe1@up.edu.ph>
2024-02-29 04:53:12 +0800

    [feat] Add working login page

--- cashmokoproj/moneyapp/urls.py
+++ cashmokoproj/moneyapp/urls.py
@@ -3,8 +3,13 @@ from . import views
 from django.contrib import admin
 from django.urls import include, path
 
+# urlpatterns = [
+#     path('', views.home, name="home"),
+#     path('login', views.login, name="login"),
+#     path('mainpage', views.mainpage, name="mainpage")
+# ]
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('login', views.login, name="login"),
-    path('mainpage', views.mainpage, name="main")
+    path('', views.home, name='home'),
+    path('login_user/', views.login_user, name='login'),
+    path('mainpage/', views.mainpage, name='mainpage'),
 ]
--- cashmokoproj/moneyapp/views.py
+++ cashmokoproj/moneyapp/views.py
@@ -2,21 +2,44 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 # Create your views here.
 def home(request):
     return render(request, "moneyapp/login.html")
 
-def login(request):
+@csrf_protect
+def login_user(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
 
-        user = User.objects.create_user(username, password)
-        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "moneyapp/mainpage.html", {"name" : username})
+        else:
+            messages.success(request, "Wrong username or password. Please try again!")
+            return redirect("home")
+    else:
+        return render(request, "moneyapp/login.html")
+    
+# def signup(request):
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         password = request.POST["password"]
 
-        messages.success(request, "Login Successful")
+#         user = User.objects.create_user(username, password)
+#         user.save()
+
+#         messages.success(request, "Login Successful")
         
-        return redirect("main")
+#         return redirect("login")
+
+@csrf_protect
 def mainpage(request):
-    pass
+    return render(request, 'moneyapp/mainpage.html')
