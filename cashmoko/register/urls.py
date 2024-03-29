90f97f2f2bc274988735c1278630ca1ef101f5a6
joshualfelipe <jlfelipe1@up.edu.ph>
2024-03-29 21:16:03 +0800

    fix: verifyEmail bug when logging in instead

--- cashmoko/register/urls.py
+++ cashmoko/register/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("verifyEmail", views.verifyEmail, name="verifyEmail"),
+    path("verifyEmail/", views.verifyEmail, name="verifyEmail"),
 ]
